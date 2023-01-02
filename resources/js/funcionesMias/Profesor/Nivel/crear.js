4d73c3a0bfdcce5abbc167f33459d3fad788060e
carlosdanielns <100952739+carlosdanielns@users.noreply.github.com>
2023-01-02 16:18:11 +0100

    nivel validacion completa

--- resources/js/funcionesMias/Profesor/Nivel/crear.js
+++ resources/js/funcionesMias/Profesor/Nivel/crear.js
@@ -1,5 +1,12 @@
+const expresiones = {
+  descripcion: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+};
+
+const campos = {
+  descripcion: false,
+};
+
 const temaCrear = JSON.parse(localStorage.getItem("temaId"));
-console.log(temaCrear);
 
 //Abrir modal Crear
 function abrirModalCrear() {
@@ -12,70 +19,137 @@ function abrirModalCrear() {
   tiempoDuracion.setAttribute("min", 10);
   $("#modalCrear").modal({ backdrop: "static", keyboard: false });
   $("#modalCrear").modal("show");
+
+  var descripcionBien = document.getElementById("descripcionBien");
+  descripcionBien.style.visibility = "hidden";
+  var notaBien = document.getElementById("notaBien");
+  notaBien.style.visibility = "hidden";
+  var timepoDuracionBien = document.getElementById("timepoDuracionBien");
+  timepoDuracionBien.style.visibility = "hidden";
   quitarDivCrear();
 }
 //Fin modal Crear
 
 //Crear Nivel
 $("#formularioCreate").on("submit", function (e) {
+  e.preventDefault();
+  const url = "http://localhost:3000/api/v2/nivel/";
+
+  if (navigator.onLine) {
+    server(url);
+    var newDescripcion = $("#descripcion");
+    var newNota5 = $("#nota5");
+    var newtiempoDuracion = $("#tiempoDuracion");
+
+    if (!expresiones.descripcion.test(newDescripcion.val())) {
+      var descripcionMal = document.getElementById("descripcionMal");
+      descripcionMal.style.visibility = "visible";
+      return;
+    }
+
+    agregar(newDescripcion.val(), newNota5.val(), newtiempoDuracion.val());
+  } else {
+    $("#modalCrear").modal("hide");
+    $("#internet").modal("show");
+  }
+});
+
+function agregar(descripcion, nota5, tiempoDuracion) {
   const url = "http://localhost:3000/api/v2/nivel/";
   let token = JSON.parse(localStorage.getItem("token"));
 
-  e.preventDefault();
   onClickBotonCrear();
-  var newDescripcion = $("#descripcion");
-  var newNota5 = $("#nota5");
-  var newtiempoDuracion = $("#tiempoDuracion");
-  console.log(newtiempoDuracion);
 
-  var data = {
-    descripcion: newDescripcion.val(),
-    nota5: newNota5.val(),
-    tiempoDuracion: newtiempoDuracion.val(),
-  };
+  if (navigator.onLine) {
+    server(url);
 
-  console.log(data);
-  fetch(url, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json, text/plain, */*",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(data),
-  })
-    .then((resPostNivel) => resPostNivel.json())
-    .then((resPostNivel) => {
-      const urlAddNivel =
-        "http://localhost:3000/api/v2/tema/" +
-        temaCrear.id +
-        "/nivel/" +
-        resPostNivel._id;
-      console.log(urlAddNivel);
-      fetch(urlAddNivel, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json, text/plain, */*",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((resAddNivel) => resAddNivel.json())
-        .then((resAddNivel) => {
-          if (resAddNivel.status == 401 || resAddNivel.statusCode == 401) {
-            $("#modal401").modal({
-              backdrop: "static",
-              keyboard: false,
-            });
-            $("#modal401").modal("show");
+    fetch(url, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text/plain, */*",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        var find = false;
+        for (let j = 0; j < res.length && find == false; j++) {
+          if (
+            res[j].descripcion == descripcion &&
+            res[j].nota5 == nota5 &&
+            res[j].tiempoDuracion == tiempoDuracion
+          ) {
+            var descripcionBien = document.getElementById("descripcionBien");
+            descripcionBien.style.visibility = "visible";
+            var notaBien = document.getElementById("notaBien");
+            notaBien.style.visibility = "visible";
+            var timepoDuracionBien =
+              document.getElementById("timepoDuracionBien");
+            timepoDuracionBien.style.visibility = "visible";
+            find = true;
           }
-        })
-        .finally(() => {
-          quitarDivCrear();
-          location.replace("/nivel/listado");
-        });
-    });
-});
+        }
+
+        if (find == false) {
+          onClickBotonCrear();
+          var data = {
+            descripcion: descripcion,
+            nota5: nota5,
+            tiempoDuracion: tiempoDuracion,
+          };
+
+          fetch(url, {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json, text/plain, */*",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(data),
+          })
+            .then((resPostNivel) => resPostNivel.json())
+            .then((resPostNivel) => {
+              const urlAddNivel =
+                "http://localhost:3000/api/v2/tema/" +
+                temaCrear.id +
+                "/nivel/" +
+                resPostNivel._id;
+              console.log(urlAddNivel);
+              fetch(urlAddNivel, {
+                method: "post",
+                headers: {
+                  "Content-Type": "application/json",
+                  Accept: "application/json, text/plain, */*",
+                  Authorization: `Bearer ${token}`,
+                },
+              })
+                .then((resAddNivel) => resAddNivel.json())
+                .then((resAddNivel) => {
+                  if (
+                    resAddNivel.status == 401 ||
+                    resAddNivel.statusCode == 401
+                  ) {
+                    $("#modal401").modal({
+                      backdrop: "static",
+                      keyboard: false,
+                    });
+                    $("#modal401").modal("show");
+                  }
+                })
+                .finally(() => {
+                  quitarDivCrear();
+                  location.replace("/nivel/listado");
+                });
+            });
+        }
+      })
+      .finally(() => {
+        quitarDivCrear();
+      });
+  }
+}
+
 // Fin Crear Nivel
 
 function onClickBotonCrear() {
@@ -89,6 +163,9 @@ function onClickBotonCrear() {
 }
 
 function quitarDivCrear() {
+  var descripcionMal = document.getElementById("descripcionMal");
+  descripcionMal.style.visibility = "hidden";
+
   var boton = document.getElementById("botonInsertar");
   var chargerInsertar = document.getElementById("chargerInsertar");
 
@@ -103,4 +180,77 @@ var close = document.getElementById("close");
 
 close.addEventListener("click", function () {
   formulario.reset();
+});
+
+function server(url) {
+  var source = new EventSource(url);
+  var isOpen = false;
+
+  source.addEventListener(
+    "message",
+    function (e) {
+      console.log(e.data);
+    },
+    false
+  );
+
+  source.addEventListener(
+    "open",
+    function (e) {
+      // Server up
+      console.log("El server esta corriendo");
+      isOpen = true;
+    },
+    false
+  );
+
+  source.addEventListener(
+    "error",
+    function (e) {
+      if (!isOpen && source.readyState == EventSource.CONNECTING) {
+        // Server down
+        $("#serverCaido").modal("show");
+      } else if (source.readyState == EventSource.CLOSED) {
+        // Server error
+      }
+      isOpen = false;
+    },
+    false
+  );
+}
+
+const validarFormulario = (e) => {
+  console.log(e);
+  switch (e.target.name) {
+    case "descripcion":
+      validarCampo(expresiones.descripcion, e.target, "descripcion");
+      break;
+  }
+};
+
+const validarCampo = (expresion, input, campo) => {
+  var descripcionMal = document.getElementById("descripcionMal");
+  var descripcionBien = document.getElementById("descripcionBien");
+  descripcionBien.style.visibility = "hidden";
+  descripcionMal.style.visibility = "hidden";
+
+  var descripcionBien = document.getElementById("descripcionBien");
+  descripcionBien.style.visibility = "hidden";
+  var notaBien = document.getElementById("notaBien");
+  notaBien.style.visibility = "hidden";
+  var timepoDuracionBien = document.getElementById("timepoDuracionBien");
+  timepoDuracionBien.style.visibility = "hidden";
+
+  if (expresion.test(input.value)) {
+    campos[campo] = true;
+  } else {
+    descripcionMal.style.visibility = "visible";
+    campos[campo] = false;
+  }
+};
+
+const inputs = document.querySelectorAll("#formularioCreate input");
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
 });
--- resources/js/funcionesMias/Profesor/Nivel/update.js
+++ resources/js/funcionesMias/Profesor/Nivel/update.js
@@ -1,9 +1,25 @@
+const expresionesUpdate = {
+  descripcion: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+};
+
+const camposUpdate = {
+  descripcionUpdate: false,
+};
+
 const temaUpdate = JSON.parse(localStorage.getItem("temaId"));
-console.log(temaUpdate);
 
 function cargarModal() {
   $("#modalUpdate").modal({ backdrop: "static", keyboard: false });
   $("#modalUpdate").modal("show");
+
+  var descripcionBienUpdate = document.getElementById("descripcionBienUpdate");
+  descripcionBienUpdate.style.visibility = "hidden";
+  var notaBienUpdate = document.getElementById("notaBienUpdate");
+  notaBienUpdate.style.visibility = "hidden";
+  var timepoDuracionBienUpdate = document.getElementById(
+    "timepoDuracionBienUpdate"
+  );
+  timepoDuracionBienUpdate.style.visibility = "hidden";
   quitarDivModificar();
 }
 
@@ -26,64 +42,159 @@ function update(id, descripcion, nota5Update, tiempoDuracionUpdate) {
 function updateNivel(id, descripcion, nota5Update, tiempoDuracionUpdate) {
   $("#formularioUpdate").on("submit", function (e) {
     e.preventDefault();
-    onClickBotonModificar();
-    let token = JSON.parse(localStorage.getItem("token"));
-
     var urlNivel = "http://localhost:3000/api/v2/nivel/";
 
-    var newDescripcion = $("#descripcionUpdate");
-    var newNota5 = $("#nota5Update");
-    var newTiempoDuracion = $("#tiempoDuracionUpdate");
+    if (navigator.onLine) {
+      server(urlNivel);
 
-    var nota5Guardar;
-    var tiempoDuracionGuardar;
+      var newDescripcion = $("#descripcionUpdate");
+      var newNota5 = $("#nota5Update");
+      var newTiempoDuracion = $("#tiempoDuracionUpdate");
 
-    if (newNota5.val() == "" && newTiempoDuracion.val() == "") {
-      nota5Guardar = nota5Update;
-      tiempoDuracionGuardar = tiempoDuracionUpdate;
-    } else if (newTiempoDuracion.val() == "") {
-      tiempoDuracionGuardar = tiempoDuracionUpdate;
-      nota5Guardar = newNota5.val();
-    } else if (newNota5.val() == "") {
-      nota5Guardar = nota5Update;
-      tiempoDuracionGuardar = newTiempoDuracion.val();
-    } else if (newNota5.val() != "" && newTiempoDuracion.val() != "") {
-      nota5Guardar = newNota5.val();
-      tiempoDuracionGuardar = newTiempoDuracion.val();
-    }
-
-    var data = {
-      descripcion: newDescripcion.val(),
-      nota5: nota5Guardar,
-      tiempoDuracion: tiempoDuracionGuardar,
-    };
-
-    console.log(data);
-
-    fetch(urlNivel + id, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json, text/plain, */*",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(data),
-    })
-      .then((resPut) => resPut.json())
-      .then((resPut) => {
-        if (resPut.status == 401 || resPut.statusCode == 401) {
-          $("#modal401").modal({
-            backdrop: "static",
-            keyboard: false,
-          });
-          $("#modal401").modal("show");
+      var descripcionOK;
+      if (newDescripcion.val() == "") {
+        descripcionOK = descripcion;
+      } else {
+        if (!expresionesUpdate.descripcion.test(newDescripcion.val())) {
+          var descripcionBien = document.getElementById("descripcionMalUpdate");
+          descripcionBien.style.visibility = "visible";
+          return;
+        } else {
+          descripcionOK = newDescripcion.val();
         }
-      })
-      .finally(() => {
-        quitarDivModificar();
-        location.replace("/nivel/listado");
-      });
+      }
+
+      var nota5Guardar;
+      var tiempoDuracionGuardar;
+
+      if (newNota5.val() == "" && newTiempoDuracion.val() == "") {
+        nota5Guardar = nota5Update;
+        tiempoDuracionGuardar = tiempoDuracionUpdate;
+      } else if (newTiempoDuracion.val() == "") {
+        tiempoDuracionGuardar = tiempoDuracionUpdate;
+        nota5Guardar = newNota5.val();
+      } else if (newNota5.val() == "") {
+        nota5Guardar = nota5Update;
+        tiempoDuracionGuardar = newTiempoDuracion.val();
+      } else if (newNota5.val() != "" && newTiempoDuracion.val() != "") {
+        nota5Guardar = newNota5.val();
+        tiempoDuracionGuardar = newTiempoDuracion.val();
+      }
+
+      modificar(
+        id,
+        descripcionOK,
+        nota5Guardar,
+        tiempoDuracionGuardar,
+        descripcion,
+        nota5Update,
+        tiempoDuracionUpdate
+      );
+    } else {
+      $("#modalUpdate").modal("hide");
+      $("#internet").modal("show");
+    }
   });
+}
+
+function modificar(
+  id,
+  descripcionOK,
+  nota5Guardar,
+  tiempoDuracionGuardar,
+  descripcion,
+  nota5Update,
+  tiempoDuracionUpdate
+) {
+  var urlNivel = "http://localhost:3000/api/v2/nivel/";
+  let token = JSON.parse(localStorage.getItem("token"));
+
+  if (navigator.onLine) {
+    server(urlNivel);
+
+    if (descripcionOK == descripcion) {
+      put(urlNivel, id, descripcionOK, nota5Guardar, tiempoDuracionGuardar);
+    } else {
+      onClickBotonModificar();
+      fetch(urlNivel, {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json, text/plain, */*",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          var find = false;
+          for (let j = 0; j < res.length && find == false; j++) {
+            if (
+              res[j].descripcion == descripcionOK &&
+              res[j].nota5 == nota5Guardar &&
+              res[j].tiempoDuracion == tiempoDuracionGuardar
+            ) {
+              var descripcionBienUpdate = document.getElementById(
+                "descripcionBienUpdate"
+              );
+              descripcionBienUpdate.style.visibility = "visible";
+              var notaBienUpdate = document.getElementById("notaBienUpdate");
+              notaBienUpdate.style.visibility = "visible";
+              var timepoDuracionBienUpdate = document.getElementById(
+                "timepoDuracionBienUpdate"
+              );
+              timepoDuracionBienUpdate.style.visibility = "visible";
+              find = true;
+            }
+          }
+
+          if (find == false) {
+            put(
+              urlNivel,
+              id,
+              descripcionOK,
+              nota5Guardar,
+              tiempoDuracionGuardar
+            );
+          }
+        })
+        .finally(() => {
+          quitarDivModificar();
+        });
+    }
+  }
+}
+
+function put(urlNivel, id, descripcionOK, nota5Guardar, tiempoDuracionGuardar) {
+  let token = JSON.parse(localStorage.getItem("token"));
+  var data = {
+    descripcion: descripcionOK,
+    nota5: nota5Guardar,
+    tiempoDuracion: tiempoDuracionGuardar,
+  };
+
+  fetch(urlNivel + id, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json, text/plain, */*",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  })
+    .then((resPut) => resPut.json())
+    .then((resPut) => {
+      if (resPut.status == 401 || resPut.statusCode == 401) {
+        $("#modal401").modal({
+          backdrop: "static",
+          keyboard: false,
+        });
+        $("#modal401").modal("show");
+      }
+    })
+    .finally(() => {
+      quitarDivModificar();
+      location.replace("/nivel/listado");
+    });
 }
 
 function onClickBotonModificar() {
@@ -97,6 +208,9 @@ function onClickBotonModificar() {
 }
 
 function quitarDivModificar() {
+  var descripcionMal = document.getElementById("descripcionMalUpdate");
+  descripcionMal.style.visibility = "hidden";
+
   var boton = document.getElementById("botonModificar");
   var chargerModificar = document.getElementById("chargerModificar");
   chargerModificar.style.visibility = "hidden";
@@ -104,3 +218,80 @@ function quitarDivModificar() {
   boton.innerHTML = "Modificar Nivel";
   boton.disabled = false;
 }
+
+function server(url) {
+  var source = new EventSource(url);
+  var isOpen = false;
+
+  source.addEventListener(
+    "message",
+    function (e) {
+      console.log(e.data);
+    },
+    false
+  );
+
+  source.addEventListener(
+    "open",
+    function (e) {
+      // Server up
+      console.log("El server esta corriendo");
+      isOpen = true;
+    },
+    false
+  );
+
+  source.addEventListener(
+    "error",
+    function (e) {
+      if (!isOpen && source.readyState == EventSource.CONNECTING) {
+        // Server down
+        $("#serverCaido").modal("show");
+      } else if (source.readyState == EventSource.CLOSED) {
+        // Server error
+      }
+      isOpen = false;
+    },
+    false
+  );
+}
+
+const validarFormularioUpdate = (e) => {
+  console.log(e);
+  switch (e.target.name) {
+    case "descripcionUpdate":
+      validarCampoUpdate(
+        expresionesUpdate.descripcion,
+        e.target,
+        "descripcionUpdate"
+      );
+      break;
+  }
+};
+
+const validarCampoUpdate = (expresion, input, campo) => {
+  var descripcionMal = document.getElementById("descripcionMalUpdate");
+  var descripcionBien = document.getElementById("descripcionBienUpdate");
+  descripcionBien.style.visibility = "hidden";
+  descripcionMal.style.visibility = "hidden";
+
+  var notaBienUpdate = document.getElementById("notaBienUpdate");
+  notaBienUpdate.style.visibility = "hidden";
+  var timepoDuracionBienUpdate = document.getElementById(
+    "timepoDuracionBienUpdate"
+  );
+  timepoDuracionBienUpdate.style.visibility = "hidden";
+
+  if (expresion.test(input.value)) {
+    camposUpdate[campo] = true;
+  } else {
+    descripcionMal.style.visibility = "visible";
+    camposUpdate[campo] = false;
+  }
+};
+
+const inputsUpdate = document.querySelectorAll("#formularioUpdate input");
+
+inputsUpdate.forEach((input) => {
+  input.addEventListener("keyup", validarFormularioUpdate);
+});
